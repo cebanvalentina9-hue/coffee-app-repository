91790a9fa59f3cae73be80ab44ece79b0a62a9c2
Valentina Ceban <cebanvalentina9@gmail.com>
2026-07-04 11:44:03 +0300

    Populate DrinkCoffee

--- src/DrinkCoffee.java
+++ src/DrinkCoffee.java
@@ -1,2 +1,15 @@
 public class DrinkCoffee {
+
+    void drinCoffee(Coffee coffee) throws TemperatureException, ConcentrationException {
+        if (coffee.getTemp() > 65) {
+            throw new TemperatureException(coffee.getTemp(), "Cafeaua e prea fierbinte. O aruncam!");
+        } else {
+            System.out.println("t: " + coffee.getTemp());
+        }
+        if(coffee.getConc() > 55){
+            throw new ConcentrationException(coffee.getConc(), "Concentratie prea mare. O aruncam");
+        } else {
+            System.out.println("c: " + coffee.getConc());
+        }
+    }
 }
